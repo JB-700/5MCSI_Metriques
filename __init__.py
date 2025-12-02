7d1f56f5f904f5ac6a0367252ca3882e7329b704
JB-700 <j.beving@myskolae.fr>
2025-12-02 10:27:48 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -59,29 +59,31 @@ def mongraphique():
 
 @app.route('/commits/')
 def commits():
+    return render_template("commits.html")
+
+@app.route('/commits/data/')
+def commits_data():
     response = urlopen('https://api.github.com/repos/JB-700/5MCSI_Metriques/commits')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
-    commits_per_minute = {}
-    for i in range(60):
-        commits_per_minute[i] = 0
+
+    commits_per_minute = {i: 0 for i in range(60)}
 
     for commit in json_content:
         date_string = commit['commit']['author']['date']
         date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
         minutes = date_object.minute
         commits_per_minute[minutes] += 1
-    
 
     results = []
     for minute, count in commits_per_minute.items():
         results.append({'minute': minute, 'count': count})
-    
+
     return jsonify(results=results)
 
 @app.route("/commits-graph/")
 def commits_graph():
-    return render_template("commits.html")
+    return commits()
 
 
 if __name__ == "__main__":
